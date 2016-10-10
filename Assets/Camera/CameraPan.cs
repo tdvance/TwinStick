f68b64a8fa48273f152cfb3f3479c5176e6d17a6
tdvance <tdvance@gmail.com>
2016-10-10 16:24:02 -0400

    Added door and frame Adjusted sizes of assets

---
+++ Assets/Camera/CameraPan.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPan : MonoBehaviour {
+    GameObject player;
+    new Camera camera;
+
+    // Use this for initialization
+    void Start() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        camera = Camera.main;
+    }
+
+    void LateUpdate() {
+        Vector3 direction = player.transform.position - camera.transform.position;
+        camera.transform.rotation = Quaternion.FromToRotation(Vector3.forward, direction);
+        float angle = -camera.transform.rotation.eulerAngles.z;
+        camera.transform.Rotate(Vector3.forward, angle);
+    }
+}
--- Assets/Game/GameManager.cs
+++ Assets/Game/GameManager.cs
@@ -6,19 +6,42 @@ using UnityStandardAssets.CrossPlatformInput;
 public class GameManager : MonoBehaviour {
     public bool recording = true;
 
-	// Use this for initialization
-	void Start () {
+    private float savedFixDeltaTime;
+
+    // Use this for initialization
+    void Start() {
         recording = true;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         if (CrossPlatformInputManager.GetButtonDown("Replay")) {
             if (recording) {
                 recording = false;
-            }else {
+            } else {
                 recording = true;
             }
         }
-	}
+        if (Input.GetKeyDown(KeyCode.P)) {
+            if (IsPaused()) {
+                ResumeGame();
+            } else {
+                PauseGame();
+            }
+        }
+    }
+    void PauseGame() {
+        Time.timeScale = 0;
+        savedFixDeltaTime = Time.fixedDeltaTime;
+        Time.fixedDeltaTime = 0;
+    }
+
+    void ResumeGame() {
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = savedFixDeltaTime;
+    }
+
+    bool IsPaused() {
+        return Time.timeScale == 0;
+    }
 }
